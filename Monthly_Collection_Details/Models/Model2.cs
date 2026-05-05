868810e31df8ba49262975891c891ce66469903a
virajpasindu <virajpasindu308@gmail.com>
2026-05-05 15:10:24 +0530

    creating controllers, models and service codes for the API

--- Monthly_Collection_Details/Models/Model2.cs
+++ Monthly_Collection_Details/Models/Model2.cs
@@ -1,6 +1,18 @@
 ﻿namespace Monthly_Collection_Details.Models
 {
-    public class Model2
+    public class ReturnChequeDetail
     {
+        public string AcctNumber { get; set; }
+        public string CheqNo { get; set; }
+        public string CheqDate { get; set; }
+        public int NoMonths { get; set; }
+        public string EntryDate { get; set; }
+        public string Allow { get; set; }
+        public string ProvCode { get; set; }
+    }
+
+    public class ReturnChequeRequest
+    {
+        public string AccountNo { get; set; }
     }
 }
